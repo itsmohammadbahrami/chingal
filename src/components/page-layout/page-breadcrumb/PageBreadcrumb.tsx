eb240f212721e5ac18e166ec14449c95e906db5e
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2023-09-25 22:37:12 +0330

    fix: display all items in breadcrumb

--- src/components/page-layout/page-breadcrumb/PageBreadcrumb.tsx
+++ src/components/page-layout/page-breadcrumb/PageBreadcrumb.tsx
@@ -1,45 +1,49 @@
 import { Breadcrumb, Typography } from 'antd'
 import { BreadcrumbItemType, BreadcrumbSeparatorType } from 'antd/es/breadcrumb/Breadcrumb'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { ArrowLeftIcon } from 'src/assets/icons'
 import { PathItem } from 'src/interfaces'
 import { path } from 'src/routes'
 
 const PageBreadcrumb = () => {
-    const { pathname } = useLocation()
-    const navigate = useNavigate();
+    const navigate = useNavigate()
+    const { id } = useParams()
 
     const items: Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] = Object.values(path).flatMap((pathItem, index) => {
         if (pathItem?.items) {
             const root: PathItem = pathItem?.root
-            return [
+            const result = [
                 {
                     title: root.title,
                     href: root.href
-                },
-                ...Object.values(pathItem.items).map((item: any) => {
-                    const { title, href }: PathItem = item
-                    return { title, href }
-                })]
+                }]
+            Object.values(pathItem.items).forEach((item: any) => {
+                const { title, href }: PathItem = item
+                if (id && href.includes(':id'))
+                    result.push({ title, href })
+            })
+
+            return result
         }
         return {
             title: '',
             href: ''
         }
     })
+
     return (
         <Breadcrumb
             className='mb-8'
             separator={<ArrowLeftIcon />}
             itemRender={(route) =>
                 <Typography.Text
-                    className='text-base cursor-pointer'
-                    onClick={() => route.href && navigate(route.href)}
+                    className={`text-base ${route.href?.includes(':id') ? 'cursor-default' : 'cursor-pointer'}`}
+                    onClick={() => !route.href?.includes(':id') && route.href && navigate(route.href)}
                 >
                     {route.title}
                 </Typography.Text>
             }
-            items={items.filter(item => item.title && pathname.includes(item.href || ''))}
+            items={items.filter(item => item.title)}
         />
     )
 }
