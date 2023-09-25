2b9372d00e7f446d26b8fbf46f177202126c0050
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2023-09-25 13:39:29 +0330

    feat: add users page

---
+++ src/pages/users/Users.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from 'src/components'
+
+const Users = () => {
+    return (
+        <PageLayout>
+            <div>Users</div>
+        </PageLayout>
+    )
+}
+
+export default Users
